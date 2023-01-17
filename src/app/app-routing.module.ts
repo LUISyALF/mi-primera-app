051bfc3c92731e41d6b9a41b6c237992960800e7
LUISyALF <luiscorberan@gmail.com>
2023-01-17 12:06:51 +0100

    17/01/23 Cambios en el repo de clase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { FacturaComponent } from './factura/factura.component';
+import { NominaComponent } from './nomina/nomina.component';
+import { CommentComponent } from './comment/comment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'empleados', component: EmpleadoComponent},
+  {path: 'facturas', component: FacturaComponent},
+  {path: 'nominas', component: NominaComponent},
+  {path: 'comments', component: CommentComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { FacturaComponent } from './factura/factura.component';
+import { NominaComponent } from './nomina/nomina.component';
+import { CommentComponent } from './comment/comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    EmpleadoComponent,
+    FacturaComponent,
+    NominaComponent,
+    CommentComponent
   ],
   imports: [
     BrowserModule,
